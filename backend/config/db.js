4a2b2928a268865b3778f63e804512b05bbd5425
Jainil Desai <178654413+jainilDesai@users.noreply.github.com>
2024-11-22 19:11:52 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(`${process.env.MOGOOSE_DB_URL}`)
+    .connect(process.env.MOGOOSE_DB_URL)
     .then(() => console.log("db connected"));
 };
